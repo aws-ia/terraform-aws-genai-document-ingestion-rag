e0d103c7987cb66ef481a23949f29aeb5dd46210
Michael Walker <michaelhuytran@gmail.com>
2024-07-01 12:18:40 +0300

    fix: clean appsync config

--- modules/document-ingestion/appsync.tf
+++ modules/document-ingestion/appsync.tf
@@ -1,6 +1,9 @@
 resource "aws_appsync_graphql_api" "ingestion_graphql_api" {
   name = "${var.app_prefix}ingestionGraphqlApi"
   schema = file("${path.module}/schema.graphql")
+
+  xray_enabled = true
+
   authentication_type = "AMAZON_COGNITO_USER_POOLS"
   user_pool_config {
       aws_region = data.aws_region.current_region.name
@@ -10,20 +13,21 @@ resource "aws_appsync_graphql_api" "ingestion_graphql_api" {
   additional_authentication_provider {
     authentication_type = "AWS_IAM"
   }
-  xray_enabled = true
+
   log_config {
-    field_log_level = "ALL"
     cloudwatch_logs_role_arn = aws_iam_role.appsync_logs_role.arn
+    field_log_level          = "ALL"
   }
 }
 
 resource "aws_appsync_datasource" "ingestion_event_bridge_datasource" {
-  api_id = aws_appsync_graphql_api.ingestion_graphql_api.id
-  name   = "_${var.app_prefix}_ingestionEventBridgeDataSource"
-  type   = "AMAZON_EVENTBRIDGE"
+  api_id           = aws_appsync_graphql_api.ingestion_graphql_api.id
+  name             = "_${var.app_prefix}_ingestionEventBridgeDataSource"
+  type             = "AMAZON_EVENTBRIDGE"
   service_role_arn = aws_iam_role.ingestion_construct_role.arn
+
   event_bridge_config {
-    event_bus_arn = aws_cloudwatch_event_bus.ingestion_event_bus.arn
+    event_bus_arn  = aws_cloudwatch_event_bus.ingestion_event_bus.arn
   }
 }
 
@@ -32,6 +36,7 @@ resource "aws_appsync_resolver" "ingest_document_resolver" {
   type        = "Mutation"
   field       = "ingestDocuments"
   data_source = aws_appsync_datasource.ingestion_event_bridge_datasource.name
+
   request_template = <<EOF
     {
       "version": "2018-05-29",
--- modules/question-answering/appsync.tf
+++ modules/question-answering/appsync.tf
@@ -1,11 +1,9 @@
 resource "aws_appsync_graphql_api" "question_answering_graphql_api" {
-  name         = "${var.app_prefix}-questionAnsweringGraphqlApi"
-  schema       = file("${path.module}/schema.graphql")
+  name = "${var.app_prefix}-questionAnsweringGraphqlApi"
+  schema = file("${path.module}/schema.graphql")
+
   xray_enabled = true
-  log_config {
-    cloudwatch_logs_role_arn = aws_iam_role.qa_construct_role.arn
-    field_log_level          = "ERROR"
-  }
+
   authentication_type = "AMAZON_COGNITO_USER_POOLS"
   additional_authentication_provider {
     authentication_type = "AWS_IAM"
@@ -15,6 +13,11 @@ resource "aws_appsync_graphql_api" "question_answering_graphql_api" {
     default_action = "ALLOW"
     user_pool_id   = var.cognito_user_pool_id
   }
+
+  log_config {
+    cloudwatch_logs_role_arn = aws_iam_role.qa_construct_role.arn
+    field_log_level          = "ERROR"
+  }
 }
 
 resource "aws_appsync_datasource" "job_status_data_source" {
@@ -23,26 +26,26 @@ resource "aws_appsync_datasource" "job_status_data_source" {
   service_role_arn = aws_iam_role.job_status_data_source_role.arn
   type             = "NONE"
 }
+
 resource "aws_appsync_resolver" "job_status_resolver" {
   api_id      = aws_appsync_graphql_api.question_answering_graphql_api.id
-  field       = "updateQAJobStatus"
   type        = "Mutation"
+  field       = "updateQAJobStatus"
   data_source = aws_appsync_datasource.job_status_data_source.name
 
   request_template = <<EOF
-  {
-    "version": "2017-02-28",
-    "payload": $util.toJson($context.args)
-  }
-EOF
-
+    {
+      "version": "2017-02-28",
+      "payload": $util.toJson($context.args)
+    }
+  EOF
   response_template = <<EOF
-  #if($ctx.result.statusCode == 200)
-    $util.toJson($context.result)
-  #else
-    $utils.appendError($ctx.result.body, $ctx.result.statusCode)
-  #end
-EOF
+    #if($ctx.result.statusCode == 200)
+      $util.toJson($context.result)
+    #else
+      $utils.appendError($ctx.result.body, $ctx.result.statusCode)
+    #end
+  EOF
 
   caching_config {
     caching_keys = [
