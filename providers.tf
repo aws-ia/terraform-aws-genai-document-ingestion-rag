77802754f3de6705ff4ff80cae187dcec9c696bc
wellsiau-aws <wellsiau@amazon.com>
2024-07-06 14:59:33 -0700

    update version

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.8.5"
+  required_version = ">= 1.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,6 +8,18 @@ terraform {
     awscc = {
       source  = "hashicorp/awscc"
       version = ">= 1.3.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">=2.5.0"
     }
   }
 }
