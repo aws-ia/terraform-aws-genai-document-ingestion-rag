d3b2574627c2c1896ae33594397e4901cd12f128
wellsiau-aws <wellsiau@amazon.com>
2024-07-25 11:04:21 -0700

    tflint

--- data.tf
+++ data.tf
@@ -1,3 +1,1 @@
-data "aws_availability_zones" "current" {}
 data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vpc" {
-  value = module.networking_resources.private_subnet_attributes_by_az
-}
-
-output "sg" {
-  value = module.networking_resources.lambda_sg
-}
