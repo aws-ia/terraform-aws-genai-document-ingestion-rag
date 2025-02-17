20c3e8145c035ae49f3a942b6d689abdf0b470bb
Dorra ELBoukari <elboukdo@amazon.fr>
2025-02-17 13:56:17 +0100

    define aws_iam_role_policy_attachments_exclusive for ingestion api ds

--- modules/document-ingestion/iam.tf
+++ modules/document-ingestion/iam.tf
@@ -58,15 +58,15 @@ data "aws_iam_policy" "AmazonEventBridgeFullAccess" {
   arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "ingestion_api_datasource_lambda_managed_policies_attach" {
-  role       = aws_iam_role.ingestion_api_datasource.name
-  policy_arn = data.aws_iam_policy.AWSLambdaBasicExecutionRole.arn
+resource "aws_iam_role_policy_attachments_exclusive" "ingestion_api_datasource_lambda_managed_policies_attach" {
+  role_name = aws_iam_role.ingestion_api_datasource.name
+  policy_arns = [
+    data.aws_iam_policy.AWSLambdaBasicExecutionRole.arn,
+    data.aws_iam_policy.AmazonEventBridgeFullAccess.arn
+  ]
 }
 
-resource "aws_iam_role_policy_attachment" "ingestion_api_datasource_eventbridge_managed_policies_attach" {
-  role       = aws_iam_role.ingestion_api_datasource.name
-  policy_arn = data.aws_iam_policy.AmazonEventBridgeFullAccess.arn
-}
+
 ############################################################################################################
 # IAM Role for Ingestion Input Validation Lambda
 ############################################################################################################
